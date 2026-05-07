1b8469ff612f6781aa149632c630ed12da039ec2
IkramEnwer <ikram304@gmail.com>
2026-05-07 04:33:05 +0200

    Fiks admin seed password og  add nginx image config

--- Migrations/20260505154606_UpdateAdminSeedPassword.cs
+++ Migrations/20260505154606_UpdateAdminSeedPassword.cs
@@ -16,21 +16,11 @@ namespace MeetSlot.Migrations
                 keyValue: 1,
                 column: "PasswordHash",
                 value: "$2a$12$uxw4qVhvc1V861Zv.0ZgEur8SbmYNz7nAslvZpxvGLKPl.SYEoet2");
-
-            migrationBuilder.InsertData(
-                table: "AppUsers",
-                columns: new[] { "Id", "Email", "PasswordHash" },
-                values: new object[] { 2, "user@meetslot.local", "$2a$11$.8xuNfskTdFy1lx9r8WR4.jHv9ElMXVy8lMEYhz6sJGrUMhyP5V02" });
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DeleteData(
-                table: "AppUsers",
-                keyColumn: "Id",
-                keyValue: 2);
-
             migrationBuilder.UpdateData(
                 table: "AppUsers",
                 keyColumn: "Id",
